050efa542c835a7db5aec32202d3db1ff126f700
Ori Levitan <orilevitan2@gmail.com>
2019-06-22 22:31:36 +0300

    Updates

---
+++ precache-manifest.220c6fc351b540eed11a9f948911ed8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319c03d93029a5914bdc85c6e2016fad",
+    "url": "/edge-books/index.html"
+  },
+  {
+    "revision": "52424180fa7733779dc6",
+    "url": "/edge-books/static/css/main.9e855d66.chunk.css"
+  },
+  {
+    "revision": "712f90a1bca7751f3e5b",
+    "url": "/edge-books/static/js/2.6772a36e.chunk.js"
+  },
+  {
+    "revision": "52424180fa7733779dc6",
+    "url": "/edge-books/static/js/main.3c5d265c.chunk.js"
+  },
+  {
+    "revision": "b7341d83d4fe05273869",
+    "url": "/edge-books/static/js/runtime~main.2e24e2fb.js"
+  }
+]);
